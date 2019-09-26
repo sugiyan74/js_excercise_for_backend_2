097db34d518efe246e78f33c952e2cff765d0d49
ksuigyama <k.sugiyama@apassist.com>
2019-09-26 21:58:05 +0900

    edit index.js

--- excercise/index.js
+++ excercise/index.js
@@ -1,1 +1,12 @@
-console.log("aa");
+const axios = require('axios').default;
+const APIURL = 'https://opentdb.com/api.php?amount=10';
+
+(async() => {
+    try {
+        const response = await axios.get(APIURL);
+        const results = response.data.results;
+        console.log('クイズデータ:', results);
+    } catch {
+        console.log('エラーメッセージ', error.message)
+    }
+})();
